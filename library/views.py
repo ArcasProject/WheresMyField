0e4e445a946771780009f81ec16b029d66c937bd
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2017-03-29 14:04:08 +0100

    Move machine learning and read in

--- library/views.py
+++ library/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, render, render_to_response
+from .machine_learning.ml_journals import MachineLearning
 
 def viewDashboard(request, slug):
     keywords = ['Game Theory', 'Prisoners Dilemma', 'Tournament']
