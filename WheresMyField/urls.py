a0683d6e88c3ceac25ab9666a6d8ebab23359890
Geraint Palmer <palmer.geraint@googlemail.com>
2017-03-29 13:21:48 +0100

    model takes in slug

--- WheresMyField/urls.py
+++ WheresMyField/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url, include
 from library import views
 
 urlpatterns = [
-    url(r'^index', views.viewDashboard),
+    # url(r'^index', views.viewDashboard),
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^index/(?P<slug>[-\w]+)/$', views.viewDashboard, name='thing_detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, render, render_to_response
 
-def viewDashboard(request):
+def viewDashboard(request, slug):
 	keywords = ['Game Theory', 'Prisoners Dilemma', 'Tournament']
 	authors = ['Nikoleta Glynatsi', 'Prince Charles', 'Postman Pat', 'Madonna']
 	journals = ['Management Science', 'Operations Research', 'EJOR']
-	return render(request, 'library/index.html', {'keywords':keywords, 'authors':authors, 'journals':journals})
+	return render(request, 'library/index.html', {'keywords':keywords, 'authors':authors, 'journals':journals, 'slug':slug})
